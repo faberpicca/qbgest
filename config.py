90209c1f5804f2b028e5d535e80f1a5768ee73f7
Emanuele Centurioni <emanuele@centu.it>
2023-03-06 11:53:24 +0100

    aggiornato il file di configurazione

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'koijonco824xmiufh83kj'
-    SQLALCHEMY_DATABASE_URI = "postgresql://user:password@myserver.it:5432/database"
+    SQLALCHEMY_DATABASE_URI = "postgresql://qbgest:secret@localhost:5432/qbgestdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SEND_FILE_MAX_AGE_DEFAULT = 0
 
